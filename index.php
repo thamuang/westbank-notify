932d4625c7c7a8c0831bd0825d9f808a5944dca7
Arthip <arthip.p@gmail.com>
2018-02-08 16:44:37 +0700

    add form

--- index.php
+++ index.php
@@ -1,46 +1,89 @@
-<?php
-  $token = getenv('NOTIFY_TOKEN');
+<!DOCTYPE html>
+<html lang="en">
 
-  $content = array('message' => 'Hello BOT !');
+<head>
+	<title>Contact</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--===============================================================================================-->
+</head>
 
-  print_r($token);
-  print_r(LineNotify::PushMessage($token,$content));
+<body>
 
-  class LineNotify
-  {
-    public function PushMessage($token, $body)
-    {
-      $result_ = "";
-      $url = 'https://notify-api.line.me/api/notify';
+	<div class="contact1">
+		<div class="container-contact1">
+			<div class="contact1-pic js-tilt" data-tilt>
+				<img src="images/img-01.png" alt="IMG">
+			</div>
 
-      $ch = curl_init();
-      curl_setopt( $ch, CURLOPT_URL, $url);
-      // SSL USE
-      curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
-      curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
-      //POST
-      curl_setopt( $ch, CURLOPT_POST, 1);
-      curl_setopt( $ch, CURLOPT_POSTFIELDS, $body);
-      curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-      //ADD header array
-      $headers = array( 'Content-type: multipart/form-data', 'Authorization: Bearer '.$token, );
-      curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
-      //RETURN
-      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+			<form action="https://westbank-notify.herokuapp.com/notify.php" method="POST" class="contact1-form validate-form">
+				<span class="contact1-form-title">
+					Get in touch
+				</span>
 
-      $result = curl_exec( $ch );
+				<div class="wrap-input1 validate-input" data-validate="Name is required">
+					<input class="input1" type="text" name="name" placeholder="Name">
+					<span class="shadow-input1"></span>
+				</div>
 
-      //Check error
-      if(curl_error( $ch )) {
-        $error_ = array('status' => "Error",'massage' => curl_error( $ch ));
-        $result_ = json_decode($error_, true);
-      }
-      else {
-        $result_ = json_decode($result, true);
-      }
-      //Close connect
-      curl_close( $ch );
-      return  $result_;
-  }
-}
-?>
+				<div class="wrap-input1 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+					<input class="input1" type="text" name="email" placeholder="Email">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate="Subject is required">
+					<input class="input1" type="text" name="subject" placeholder="Subject">
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="wrap-input1 validate-input" data-validate="Message is required">
+					<textarea class="input1" name="message" placeholder="Message"></textarea>
+					<span class="shadow-input1"></span>
+				</div>
+
+				<div class="container-contact1-form-btn">
+					<button class="contact1-form-btn">
+						<span>
+							Send Email
+							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script>
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+	<!--===============================================================================================-->
+	<!-- <script src="js/main.js"></script> -->
+
+</body>
+
+</html>
